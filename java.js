71b3042539eac36f51db9e02be1020c8905221da
Mel Gross <32377080+mel-gross@users.noreply.github.com>
2018-03-23 03:03:34 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,58 @@
+// //the sliders
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// output.innerHTML = slider.value; // Display the default slider value
+
+// // Update the current slider value (each time you drag the slider handle)
+// slider.oninput = function() {
+//     output.innerHTML = this.value;
+// } 
+
+// //font managing 
+// var fontA = new FontFaceObserver('ansonregular');
+
+// Promise.all([fontA.load()).then(function () {
+//     document.documentElement.className += " fonts-loaded";
+//     // Optimization for Repeat Views
+//     sessionStorage.foutFontsLoaded = true;
+// });
+// if (sessionStorage.foutFontsLoaded) {
+//     document.documentElement.className += " fonts-loaded";
+//     return;
+//   }
+
+// const openTrigger= document.getElementById('openTrigger');
+// openTrigger.addEventListener("click", function(){
+//     var playlistcontainer = getElementById('playlistQuiz')
+//     function openQuiz(
+//         playlistcontainer.style.visibility = 'visible';
+//     );
+// },false);
+
+// var energy = document.getElementById('slidecontainer');
+// var energyOutput = energy.value;
+// var company = document.getElementById('company');
+// var companyOutput = company.value;
+// var genre = document.getElementById('genre');
+// var genreOutput = genre.value;
+// var language = document.getElementById('language');
+// var languageOutput = language.value;
+
+// energy.addEventListener("submit",function(){
+//     console.log(energyOutput);
+//     console.log(companyOutput);
+//     console.log(genreOutput);
+//     console.log(languageOutput);
+// }, false);
+
+
+
+// if a, b or c is clicked then append playlistN to the options List
+// if clicked again, undo action 
+var __ =document.getElementById;
+var log_= console.log;
+const LightRock= __(;LightRock);
+
+log_(__);
+log_(log_);
+log_(LightRock);
